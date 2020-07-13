995fb46d9d3254fdda60d1515eef1f6563bc2154
Hasan Demirtaş <utsukushihito@outlook.com>
2020-07-13 13:40:57 +0300

    test test

--- lib/justpushit.rb
+++ lib/justpushit.rb
@@ -1,1 +1,10 @@
-puts ARGV.inspect
+inspect = ARGV.join(' ')
+commit_message = ''
+if inspect.empty?
+  puts 'Enter your commit message...'
+  commit_message = gets
+else
+  commit_message = inspect
+end
+puts exec("git commit -am \"" + commit_message + "\"")
+puts exec("git push")
